36b3e108e25458bbde1155ee70e9e64f9eae6dd3
Simon Larsén <slarse@kth.se>
2019-11-13 15:06:20 +0100

    [fact] Refactor git.cat_file

--- src/_pygitviz/git.py
+++ src/_pygitviz/git.py
@@ -1,14 +1,23 @@
 """Utility functions for interacting with Git."""
 import sys
 import pathlib
+import enum
+import zlib
+import itertools
 from typing import List
 from collections import namedtuple
+
 
 from _pygitviz import util
 from _pygitviz import gitobject
 from _pygitviz.gitobject import Type
 
 Ref = namedtuple("Ref", "name value".split())
+
+
+class CatFileOption(enum.Enum):
+    TYPE = "-t"
+    PRETTY = "-p"
 
 
 def collect_objects(git_root: pathlib.Path) -> List[gitobject.GitObject]:
@@ -21,14 +30,16 @@ def collect_objects(git_root: pathlib.Path) -> List[gitobject.GitObject]:
     object_dirs = (d for d in objects_root.iterdir() if len(d.name) == 2 and d.is_dir())
     object_shas = (d.name + file.name for d in object_dirs for file in d.iterdir())
     git_objects = {
-        sha: gitobject.GitObject(sha=sha, obj_type=Type(cat_file(sha, "-t")))
+        sha: gitobject.GitObject(
+            sha=sha, obj_type=Type(cat_file(sha, git_root, CatFileOption.TYPE))
+        )
         for sha in object_shas
     }
     for obj in git_objects.values():
         if obj.obj_type == Type.TREE:
-            _add_children(obj, git_objects)
+            _add_children(obj, git_objects, git_root)
         elif obj.obj_type == Type.COMMIT:
-            _add_parents_and_tree(obj, git_objects)
+            _add_parents_and_tree(obj, git_objects, git_root)
     return git_objects.values()
 
 
@@ -61,25 +72,34 @@ def state(git_root):
     """Return a hash of the current state of the .git directory. Only considers
     fsck verbose output and refs.
     """
-    rc, stdout, stderr = util.captured_run(*"git fsck --full -v".split())
+    rc, stdout, stderr = util.captured_run(*"git fsck --full -v".split(), cwd=git_root)
     refs = "".join([ref.name + ref.value for ref in collect_refs(git_root)])
     return hash(stdout + stderr + refs)
 
 
-def cat_file(sha: str, *options) -> str:
-    """Run `git cat-file` on a Git object."""
-    options = " ".join(options)
-    rc, stdout, stderr = util.captured_run(*f"git cat-file {options} {sha}".split())
+def _is_hex(s: str) -> bool:
+    try:
+        int(s, 16)
+    except ValueError:
+        return False
+    return True
+
+
+def cat_file(sha: str, git_root: pathlib.Path, opt: CatFileOption) -> str:
+    """Run operations similar to `git cat-file` on a Git object."""
+    rc, stdout, stderr = util.captured_run(
+        *f"git cat-file {opt.value} {sha}".split(), cwd=git_root
+    )
     if rc != 0:
-        raise RuntimeError(f"'git cat-file' exited with non-zero exit status for {sha}")
+        raise RuntimeError(stderr.strip())
     return stdout.strip()
 
 
-def _add_parents_and_tree(commit, git_objects):
+def _add_parents_and_tree(commit, git_objects, git_root):
     """Add parent reference (i.e. to the parent commit) and tree reference (to
     the top-level tree) to a commit object.
     """
-    content = cat_file(commit.sha, "-p")
+    content = cat_file(commit.sha, git_root, CatFileOption.PRETTY)
     ptr_str = content.split("\n")[0].strip()
     ptr_obj = git_objects[ptr_str.strip().split()[1]]
     commit.add_child("", ptr_obj)
@@ -93,9 +113,9 @@ def _add_parents_and_tree(commit, git_objects):
             commit.add_parent(git_objects[parent_sha])
 
 
-def _add_children(tree, git_objects):
+def _add_children(tree, git_objects, git_root):
     """Add children to a tree git object."""
-    for line in cat_file(tree.sha, "-p").split("\n"):
+    for line in cat_file(tree.sha, git_root, CatFileOption.PRETTY).split("\n"):
         *_, sha, name = line.strip().split()
         child = git_objects[sha]
         tree.add_child(name, child)
